d56664255806d0ba59ba168dc4f7ade0a9fbe791
Henrique <henrique.plima1@gmail.com>
2019-06-16 01:23:05 -0300

    Updates

---
+++ precache-manifest.892f512f4476ce4cd34dfee63c376a2b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "682bb9367e549f437c853f37735c5fa3",
+    "url": "/AulaReact/index.html"
+  },
+  {
+    "revision": "7797af8a1fe464fb97ba",
+    "url": "/AulaReact/static/css/main.8b2976e5.chunk.css"
+  },
+  {
+    "revision": "d21af38140ec4b54c6aa",
+    "url": "/AulaReact/static/js/2.0d246edb.chunk.js"
+  },
+  {
+    "revision": "7797af8a1fe464fb97ba",
+    "url": "/AulaReact/static/js/main.86f4833e.chunk.js"
+  },
+  {
+    "revision": "25923667afc581840296",
+    "url": "/AulaReact/static/js/runtime~main.7261f266.js"
+  }
+]);
--- src/utils/mask.js
+++
@@ -1,9 +0,0 @@
-const maskCurrency = (value) => {
-    
-    var newString = 'R$ ' + value.toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
-    newString = newString.replace(",", ";");
-    newString = newString.replace(".", ",");
-    return newString = newString.replace(";", ".");
-     
-}
-export default maskCurrency;
